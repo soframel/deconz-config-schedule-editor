11f26e7120bb098ed6eabe57a055fb1b6c18d474
soframel <sophie@massen-ramel.net>
2023-10-15 18:42:05 +0200

    script corrections

--- scripts/schedule-editor-start-jvm.sh
+++ scripts/schedule-editor-start-jvm.sh
@@ -1,4 +1,4 @@
 export SCHEDULE_EDITOR_LOG_LEVEL=FINEST
 cd /home/openhabian/deconz-config-schedule-editor/
-deconz-config-schedule-editor-0.0.1-SNAPSHOT-runner > /var/lib/openhab/schedule-editor.log 2>&1 &
+java -jar quarkus-run.jar > /var/lib/openhab/schedule-editor.log 2>&1 &
 echo $! > /var/lib/openhab/schedule-editor.pid
