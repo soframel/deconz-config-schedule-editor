a7b158daf80791a9d6294e35ed722405fa927568
soframel <sophie@massen-ramel.net>
2023-11-01 16:35:10 +0100

    clean native scripts

--- build-native.sh
+++
@@ -1,4 +0,0 @@
-export JAVA_HOME="/opt/mandrel-java21-23.1.0.0-Final"
-export GRAALVM_HOME="${JAVA_HOME}"
-export PATH="${JAVA_HOME}/bin:${PATH}"
-mvn package -Pnative
--- scripts/schedule-editor-start-jvm.sh
+++
@@ -1,4 +0,0 @@
-export SCHEDULE_EDITOR_LOG_LEVEL=FINEST
-cd /home/openhabian/deconz-config-schedule-editor/
-java -jar quarkus-run.jar > /var/lib/openhab/schedule-editor.log 2>&1 &
-echo $! > /var/lib/openhab/schedule-editor.pid
